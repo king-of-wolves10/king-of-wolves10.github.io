420fe41f733ad00a9c25b7e7f4b364cb5738835b
King of Wolves <safewrdg3rgreg@gmail.com>
2024-08-04 13:48:27 -0400

    Add files via upload

---
+++ botprevention.js
@@ -0,0 +1,7 @@
+//Thanks Pokemon Peninsula
+
+var footer = document.getElementById("footer");
+var address = "safewrdg3rgreg"
+var site = "gmail.com"
+
+footer.innerHTML = footer.innerHTML + "| <a href='mailto:" + address + "@" + site + "'>E-Mail</a>"
