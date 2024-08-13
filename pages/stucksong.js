a3b67cbafa448c4d36b47b460bd571c5c8efe541
King of Wolves <safewrdg3rgreg@gmail.com>
2024-08-13 19:48:43 -0400

    Update stucksong.js

--- pages/stucksong.js
+++ pages/stucksong.js
@@ -1,7 +1,7 @@
 //This is just a way to quickly edit the Song of the Day without digging in the HTML
 
 audiotag = document.getElementById("song");
-nametag = document.getElementById("songname");
+songtitle = document.getElementById("songname");
 
 var songname = "Dragon Ball Z, Tenkaichi Tag Team/Budokai HD Collection: Options/Game Select";
 var songsrc = "";
@@ -9,9 +9,9 @@ var songsrc = "";
 nametag.addEventListener("load",function(){
 
   if(songsrc != "songoftheday.mp3"){
-  nametag.innerText = songname + "\n" + "Audio source: " + songsrc;
+  songtitle.innerText = songname + "\n" + "Audio source: " + songsrc;
   }else{
-   nametag.innerText = songname + "\n" + "Audio source: Here"; 
+   songtitle.innerText = songname + "\n" + "Audio source: Here"; 
   }
   audiotag.style.display = block;
   audiotag.src = songsrc
