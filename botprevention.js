a02769518d05e4e9e303ee4fb89112d7f4c3dfe7
King of Wolves <safewrdg3rgreg@gmail.com>
2024-09-11 22:15:25 -0400

    Update botprevention.js
    updated code to display email before neopets info in aboutme.html

--- botprevention.js
+++ botprevention.js
@@ -1,7 +1,11 @@
-//Thanks Pokemon Peninsula
+//Thanks Pokemon Peninsula pokemon.ryux.net
 
 var footer = document.getElementById("footer");
+var add = document.getElementById("mail")
 var address = "safewrdg3rgreg"
 var site = "gmail.com"
 
+if(add){add.innerHTML = "<a href='mailto:" + address + "@" + site + "'>E-Mail</a>" }
+else{
 footer.innerHTML = footer.innerHTML + "| <a href='mailto:" + address + "@" + site + "'>E-Mail</a>"
+}
