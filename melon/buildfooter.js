3a15d713a3a26dc75634f3dfd8b405fbc89f1373
King of Wolves <safewrdg3rgreg@gmail.com>
2024-12-22 16:27:14 -0500

    Delete melon directory

--- melon/buildfooter.js
+++
@@ -1,5 +0,0 @@
-
-document.write(`
-<div style="display:inline-block; text-align:center; width:min-content"><img src="../link.gif"></img><br><a style="cursor: copy; " onclick=" copycode(0,this); ">Copy Code</a> </div>
-<div style="display:inline-block; text-align:center; width:min-content"><img src="link.gif"></img><br><a style="cursor: copy; " onclick=" copycode(1,this); ">Copy Code</a> </div>
-`);
